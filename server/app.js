2148338510ae23406b04cba208c62fb27667035f
Douglas Everton de Oliveira <douglas.oliveirabh@gmail.com>
2016-11-24 00:11:14 -0200

    Add configuration file and api routes

--- server/app.js
+++ server/app.js
@@ -1,19 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
-import users from './routes/users';
+import api from './routes/api';
+
 
 const app = new express();
 
 //set request log middleware
 app.use(morgan("dev"));
 
+
 //set view engine
 app.set('view engine', 'ejs');
 
-app.use('/users', users);
-
-app.get("/", function(req, res){
-    res.json({teste:'teste'});
-});
+app.use('/api', api);
 
 export default app;
---
+++ server/config/env/development.js
@@ -0,0 +1,17 @@
+export default {
+    database: {
+        connection_string : 'mongodb://douglas:isabelle@ds057816.mlab.com:57816/controle-financeiro'
+    },
+    logging: {
+        format: 'tiny',
+        options: {}
+    },   
+    mailer: {
+         from : 'SENDER EMAIL ADDRESS',
+        service: 'SERVICE_PROVIDER', // Gmail, SMTP
+        auth: {
+            user: 'EMAIL_ID',
+            pass: 'PASSWORD'
+        }
+    },
+}
